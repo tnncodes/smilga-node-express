3b62838eb0a682015f9922248bcad34c9b397daf
theandersonn <theandersonn@gmail.com>
2021-08-02 21:53:48 -0300

    Add 03-modules - Alternative Syntax

--- 01-node-tutorial/03-modules/index.js
+++ 01-node-tutorial/03-modules/index.js
@@ -3,6 +3,9 @@
 
 const names = require('./names')
 const sayHi = require('./utils')
+const data = require('./alternative-flavor')
 
 sayHi(names.john)
 sayHi(names.peter)
+
+console.log('data:', data)
