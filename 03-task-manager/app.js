46b6b21ed349074f1947a1b98c347c4bd631e270
anderson.conceicao <anderson.conceicao@b2wdigital.com>
2021-08-21 21:12:05 -0300

    task-manager: getAllTasks

--- 03-task-manager/app.js
+++ 03-task-manager/app.js
@@ -1,1 +1,19 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+
+// middleaware
+app.use(express.json());
+
+// routes
+app.use('/api/v1/tasks', tasks);
+
+// app.get('/api/v1/tasks') - get all the tasks
+// app.post('/api/v1/tasks') - create a new task
+// app.get('/api/v1/tasks/:id') - get single task
+// app.patch('/api/v1/tasks/:id') - update task
+// app.delete('/api/v1/tasks/:id') - delete task
+
+const port = 3000;
+
+app.listen(port, console.log(`🚀 Server is listening on port ${port}...`))
