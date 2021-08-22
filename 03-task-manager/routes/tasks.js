4fa0c91fa5d64db80bf75a6c180cf8a77ee73aec
anderson.conceicao <anderson.conceicao@b2wdigital.com>
2021-08-22 12:25:22 -0300

    task-manager: All Routes

--- 03-task-manager/routes/tasks.js
+++ 03-task-manager/routes/tasks.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllTasks } = require('../controllers/tasks');
+const { 
+  getAllTasks, 
+  getAllTasks, 
+  createTask, 
+  getTask, 
+  updateTask, 
+  deleteTask 
+} = require('../controllers/tasks');
 
-router.route('/').get(getAllTasks);
+router.route('/').get(getAllTasks).post(createTask);
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
 
 module.exports = router;
