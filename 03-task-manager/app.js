b29f2ba345ab4a1d26ef5a83882dbb3da44a9819
anderson.conceicao <anderson.conceicao@b2wdigital.com>
2021-08-23 22:09:02 -0300

    task-manager: add frontend

--- 03-task-manager/app.js
+++ 03-task-manager/app.js
@@ -5,16 +5,11 @@ const connectDB = require('./db/connect');
 require('dotenv').config();
 
 // middleaware
+app.use(express.static('./public'));
 app.use(express.json());
 
 // routes
 app.use('/api/v1/tasks', tasks);
-
-// app.get('/api/v1/tasks') - get all the tasks
-// app.post('/api/v1/tasks') - create a new task
-// app.get('/api/v1/tasks/:id') - get single task
-// app.patch('/api/v1/tasks/:id') - update task
-// app.delete('/api/v1/tasks/:id') - delete task
 
 const port = 3000;
 const start = async () => {
