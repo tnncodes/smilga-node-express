91614b7b873a41601ff6f159bd9cc3ae4c3bf879
anderson.conceicao <anderson.conceicao@b2wdigital.com>
2021-08-25 21:00:00 -0300

    store-api: selecy option

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query
+  const { featured, company, name, sort, fields } = req.query
   const queryObject = {}
 
   if (featured) {
@@ -28,6 +28,11 @@ const getAllProducts = async (req, res) => {
     result = result.sort(sortList)
   } else {
     result = result.sort(createAt)
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ')
+    result = result.select(fieldsList)
   }
 
   const products = await result
