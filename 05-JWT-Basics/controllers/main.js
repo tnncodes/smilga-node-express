79d59eaafd5316b1cbf7a24cee4f45360f44d889
anderson.conceicao <anderson.conceicao@b2wdigital.com>
2021-08-27 10:45:53 -0300

    jwt-basics: sign jwt

--- 05-JWT-Basics/controllers/main.js
+++ 05-JWT-Basics/controllers/main.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const CustomAPIError = require('../errors/custom-error')
 
 const login = async (req, res) => {
@@ -7,7 +8,13 @@ const login = async (req, res) => {
     throw new CustomAPIError('Please provide email and password', 400)
   }
 
-  res.send('Fake Login/Register/Signup Route')
+  // just for demo, normally provided by DB
+  const id = new Date().getDate()
+
+  // try to keep payload small, better experience for user
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {expiresIn: '30d'})
+
+  res.status(200).json({ msg: 'user created', token })
 }
 
 const dashboard = async (req, res) => {
