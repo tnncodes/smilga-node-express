e81b40ce93bd1035b1defb78565e580f21f3ff0c
anderson.conceicao <anderson.conceicao@b2wdigital.com>
2021-08-30 13:10:58 -0300

    jobs-api: compare password

--- 06-jobs-api/controllers/auth.js
+++ 06-jobs-api/controllers/auth.js
@@ -12,13 +12,19 @@ const register = async (req, res) => {
 const login = async (req, res) => {
   const { email, password } = req.body
 
-  if(!email || !password) {
-    throw new BadRequestError('Please provide email and password')
-  }
+  // if(!email || !password) {
+  //   throw new BadRequestError('Please provide email and password')
+  // }
 
   const user = await User.findOne({ email })
 
   if (!user) {
+    throw new UnauthenticatedError('Invalid Credentials') 
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password)
+
+  if (!isPasswordCorrect) {
     throw new UnauthenticatedError('Invalid Credentials')
   }
 
