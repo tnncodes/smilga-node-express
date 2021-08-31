c11252ccdc1083893cb705cf3f648f3a3de929f4
anderson.conceicao <anderson.conceicao@b2wdigital.com>
2021-08-31 12:49:16 -0300

    jobs-api: security setup

--- 06-jobs-api/app.js
+++ 06-jobs-api/app.js
@@ -1,5 +1,12 @@
 require('dotenv').config()
 require('express-async-errors')
+
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require('express')
 const app = express()
 
@@ -17,7 +24,10 @@ const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 app.use(express.json())
-// extra packages
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+app.use(rateLimiter({ windowMs: 60 * 1000, max: 60 }))
 
 // routes
 app.use('/api/v1/auth', authRouter)
